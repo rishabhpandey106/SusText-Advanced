09ef7ba98a8e99f340e17154db991079e5c00968
rishabhpandey106 <rishabhpandey230@gmail.com>
2024-02-26 23:12:20 +0530

    updated-cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 // const io = new Server(8000, {
 //   cors: true,
